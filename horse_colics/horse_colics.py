d397a57e5a7c82a103748e7c8b1d136c229857b2
Dong-hee Na <corona10@gmail.com>
2016-07-10 02:06:01 +0900

    fix

--- horse_colics/horse_colics.py
+++ horse_colics/horse_colics.py
@@ -1,34 +1,21 @@
-
 # coding: utf-8
-
-# In[380]:
-
 import tensorflow as tf
 import numpy as np
-
-
-# In[381]:
 
 def accuerncy(hypo, test):
     total = float(len(hypo))
     hit = 0.0
     for i in range(len(hypo)):
         if int(hypo[i]) == int(test[i]):
-            hit = hit + 1
-    
+            hit = hit + 1    
     return hit / total
 
-
-# In[382]:
-
+# one-hot encoding 생성
 def make_one_hot(y_data):
     y_one_hot = np.zeros([len(y_data), 2])
     for i in range(len(y_data)):
         y_one_hot[i][y_data[i]] = 1
     return y_one_hot
-
-
-# In[383]:
 
 data = np.genfromtxt('horse/horseColicTraining.txt',delimiter= '\t')
 data[np.isnan(data)] = 0.0
@@ -39,30 +26,15 @@ Y = tf.placeholder(tf.float32, [None, 2])
 Weight = tf.Variable(tf.random_uniform([21,2], -1.0, 1.0))
 bias  = tf.Variable(tf.random_uniform([1,2], -1.0, 1.0))
 
-
-# In[384]:
-
-#one hot encoding 생성
 y_one_hot = make_one_hot(y_data)
-
-
-# In[385]:
-
 hypo= tf.nn.softmax(tf.matmul(X, Weight) + bias) 
 cross_entropy = -tf.reduce_sum(Y*tf.log(hypo + 1e-12))#-tf.reduce_sum(Y*tf.log(hypo))
-
-
-# In[386]:
 
 optimizer = tf.train.AdagradOptimizer(0.05)
 train = optimizer.minimize(cross_entropy)
 
-
-# In[390]:
-
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
-#print sess.run(hypo,  feed_dict={X:x_data, Y:y_one_hot})
 rate = 5000
 print "cross_entrophy: ",sess.run(cross_entropy, feed_dict={X:x_data, Y:y_one_hot})
 for step in xrange(rate):
@@ -72,9 +44,6 @@ for step in xrange(rate):
 print "cross_entrophy: ",sess.run(cross_entropy, feed_dict={X:x_data, Y:y_one_hot})
 print "weight: ", sess.run(Weight)
 print "bias: ", sess.run(bias)
-
-
-# In[370]:
 
 data = np.genfromtxt('horse/horseColicTest.txt',delimiter= '\t')
 data[np.isnan(data)] = 0.0
@@ -87,13 +56,6 @@ all= sess.run(tf.arg_max(all,1))
 print "accurency: ",accuerncy(all, y_data)
 print all
 
-
-# In[371]:
-
 sess.close()
-
-
-# In[ ]:
-
 
 
